83988c74dcd04d17a1491e09e3d05bceb4789266
Joss <jossome.one@gmail.com>
2017-11-17 00:18:13 -0500

    Finish basic idea of tombstone, but don't work yet.

--- tombstones.h
+++ tombstones.h
@@ -10,10 +10,23 @@ template <class T> class Pointer;
 template <class T> void free(Pointer<T>& obj);
 
 template <class T>
+void dangling_pointer_error(Pointer<T>& obj) {
+    std::cerr << "Dangling reference, address: " << (obj.ptr)->content << std::endl;
+    exit(1);
+    //std::terminate();
+}
+
+template <class T>
+struct Tomb {
+    
+    T* content;
+    int ref_cnt;
+};
+
+template <class T>
 class Pointer {
-private:
-    T* ptr;
 public:
+    Tomb<T>* ptr;
     Pointer<T>();                               // default constructor
     Pointer<T>(Pointer<T>&);                        // copy constructor
     Pointer<T>(T*);                             // bootstrapping constructor
@@ -38,68 +51,94 @@ public:
 
 template <class T>
 Pointer<T>::Pointer() {
-    ptr = new T();
+    Tomb<T> tomb;
+    tomb.content = NULL;
+    tomb.ref_cnt = 0;
+    ptr = &tomb;
 }
 
 template <class T>
 Pointer<T>::Pointer(Pointer<T> &p) {
-    if (!p.getPointer()) ptr = NULL;
-    else { ptr = new T; *ptr = *(p.getPointer()); }
+    ptr = p.ptr;
+    if (!(ptr->content)) {
+        (ptr->ref_cnt)++;
+    }
+
 }
 
 template <class T>
 Pointer<T>::Pointer(T* p) {
-    if (!p) ptr = NULL;
-    else { ptr = new T; *ptr = *p; }
+    // if (!p) ptr =NULL;
+    // else { ptr = new T; *ptr = *p; }
+    Tomb<T> tomb;
+    tomb.content = p;
+    std::cout << "bootstrap: " << (p == NULL) << (tomb.content == NULL);
+    if (!(tomb.content)) {
+        tomb.ref_cnt = 0;
+    }
+    else {
+        tomb.ref_cnt = 1;
+    }
+    ptr = &tomb;
 }
 
 template <class T>
 Pointer<T>::~Pointer() {
-    if (!ptr) delete ptr;
+    // Need revise
+    if (ptr->ref_cnt > 0) delete ptr;
 }
 
 template <class T>
 T& Pointer<T>::operator*() const {
-    return *ptr;
+    return *(ptr->content);
 }
 
 template <class T>
 T* Pointer<T>::operator->() const {
-    return ptr;
+    return ptr->content;
 }
 
-// I don't know if this is assignment of reference or value.
 template <class T>
 Pointer<T>& Pointer<T>::operator=(const Pointer<T>& assignment) {
-    ptr = assignment.getPointer();
+    ptr = assignment.ptr;
+    if (ptr) {
+        ptr->ref_cnt++;
+    }
     return *this;
 }
 
 template <class T>
 bool Pointer<T>::operator==(const Pointer<T>& other) const {
-    return ptr == other.getPointer();
+    return ptr->content == (other.ptr)->content;
 }
 
 template <class T>
 bool Pointer<T>::operator!=(const Pointer<T>& other) const {
-    return ptr != other.getPointer();
+    return ptr->content != (other.ptr)->content;
 }
 
 // Generally, "new" will not return a NULL pointer,
 // so the two overload here will return a not NULL result.
 template <class T>
 bool Pointer<T>::operator==(const int comp) const {
-    return (!ptr && comp == 0) ? 1 : 0;
+    return (!(ptr->content) && comp == 0) ? 1 : 0;
 }
 
 template <class T>
 bool Pointer<T>::operator!=(const int comp) const {
-    return (!ptr && comp == 0) ? 0 : 1;
+    return (!(ptr->content) && comp == 0) ? 0 : 1;
 }
 
 template <class T>
 void free(Pointer<T>& obj) {
-    obj.~Pointer();
+    std::cout << "Freeing address: " << (obj.ptr)->content << std::endl;
+    if (!((obj.ptr)->content)) {
+        dangling_pointer_error(obj);
+    }
+    else {
+        free((obj.ptr)->content);
+        (obj.ptr)->content = NULL;
+    }
 }
 
 
